f0ef6dd28bd1c7e7bd6213b150b619a0965718dd
Alex Berry <highwaybobbery@gmail.com>
2017-09-17 10:55:26 -0700

    refactored integration spec form fill

--- spec/word_integration_spec.rb
+++ spec/word_integration_spec.rb
@@ -9,39 +9,66 @@ describe("Word") do
   end
 
   describe('My word list', {:type => :feature}) do
-    it ('processes the user entry and returns a list of words') do
+    before do
       visit('/')
-      fill_in('word', :with => 'continent')
-      fill_in('definition_1', :with => 'one of the seven main landmasses on the Earth')
-      fill_in('definition_2', :with => 'a large landmass on another planet')
-      fill_in('definition_3', :with => 'the mainland of Europe')
+      add_word(
+        word: 'continent',
+        definitions: [
+          'one of the seven main landmasses on the Earth',
+          'a large landmass on another planet',
+          'the mainland of Europe',
+        ],
+      )
       click_button('Add word!')
+    end
+
+    it ('processes the user entry and returns a list of words') do
       expect(page).to have_content('continent')
     end
   end
 
   describe('a word link', {:type => :feature}) do
-    it ('navigates to an individual word page') do
+    before do
       visit('/')
-      fill_in('word', :with => 'continent')
-      fill_in('definition_1', :with => 'one of the seven main landmasses on the Earth')
+      add_word(
+        word: 'continent',
+      )
       click_button('Add word!')
       click_link("continent")
+    end
+
+    it ('navigates to an individual word page') do
       expect(page).to have_content('continent')
     end
   end
 
   describe('a word page', {:type => :feature}) do
-    it ('displays multiple definitions if entered') do
+    before do
       visit('/')
-      fill_in('word', :with => 'continent')
-      fill_in('definition_1', :with => 'one of the seven main landmasses on the Earth')
-      fill_in('definition_2', :with => 'a large landmass on another planet')
-      fill_in('definition_3', :with => 'the mainland of Europe')
+      add_word(
+        word: 'continent',
+        definitions: [
+          'one of the seven main landmasses on the Earth',
+          'a large landmass on another planet',
+          'the mainland of Europe',
+        ],
+      )
       click_button('Add word!')
       click_link("continent")
-      expect(page).to have_content('continent', 'landmass', "Europe")
     end
+
+    it ('displays multiple definitions if entered') do
+      expect(page).to have_content('continent')
+      expect(page).to have_content('Europe')
+      expect(page).to have_content('landmass')
+    end
+  end
+
+  def add_word(word:, definitions: [])
+    fill_in('word', with: word)
+    fill_in('definition_1', with: definitions[0])
+    fill_in('definition_2', with: definitions[1])
+    fill_in('definition_3', with: definitions[2])
   end
 
 end #Word class
